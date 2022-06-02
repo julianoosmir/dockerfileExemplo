c1189e0418e30e977141894ecb7064deacb54da1
juliano <julianoosmir@outlook.com>
2022-06-01 22:44:16 -0400

    initial

---
+++ src/test/java/com/juliano/dockerimage/DockerimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliano.dockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
